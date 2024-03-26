43342d9c7f121ca779d278d4707430571bc06cbc
SamuelAsimov <99033930+SamuelAsimov@users.noreply.github.com>
2024-03-26 17:43:40 -0300

    Update Outros.py

--- pages/Outros.py
+++ pages/Outros.py
@@ -9,7 +9,7 @@ st.set_page_config(layout='wide', page_title="Saude")
 st.title("Dados sobre saude")
 st.divider()
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10 = st.tabs(["Acidentes de Trabalho", "Botulismo", "Cancer", "Desease", "Difteria", "Intoxicação", "Kombate", "Lepstopirose", "Meningite", "Tuberculose"])
+tab1, tab2 = st.tabs(["Acidentes de Trabalho", "Botulismo", "Cancer", "Desease", "Difteria", "Intoxicação", "Kombate", "Lepstopirose", "Meningite", "Tuberculose"])
 
 with tab1:
     st.markdown('<iframe src="https://app.powerbi.com/view?r=eyJrIjoiYWNjOWY1NzYtZmZjMi00MjliLTk5MDMtNDU4Yzk1ZDE5ZTIxIiwidCI6ImFkYWMzNzYyLWYzMWQtNDliNS1iYWI1LWY3NjcxNzZmZjQyNSJ9" width="100%" height="600"></iframe>', unsafe_allow_html=True)
