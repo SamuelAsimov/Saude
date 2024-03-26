6997c9a9daa7f8e8f72a44dba0292ef5f4d55959
SamuelAsimov <99033930+SamuelAsimov@users.noreply.github.com>
2024-03-26 17:43:20 -0300

    Update SIA.py

--- pages/SIA.py
+++ pages/SIA.py
@@ -9,7 +9,7 @@ st.set_page_config(layout='wide', page_title="Saude")
 st.title("Dados sobre o SIA")
 st.divider()
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10 = st.tabs(["Dialitico", "Medicamentos", "Radio", "Producão ambulatorial"])
+tab1, tab2, tab3, tab4= st.tabs(["Dialitico", "Medicamentos", "Radio", "Producão ambulatorial"])
 
 with tab1:
     st.markdown('<iframe src="https://app.powerbi.com/view?r=eyJrIjoiYjdlNGNhNTItOGJkMS00YTZkLTk5MjYtYTVmZmFlZDM5NmE0IiwidCI6ImFkYWMzNzYyLWYzMWQtNDliNS1iYWI1LWY3NjcxNzZmZjQyNSJ9" width="100%" height="600"></iframe>', unsafe_allow_html=True)
